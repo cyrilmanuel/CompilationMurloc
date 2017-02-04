9cde4d356015330b926f2ce446f25d5a9a21bb8f
cyrilmanuel <cyriljeanneret@gmail.com>
2017-02-04 13:18:41 +0100

    reprise des fichier tp3 et ajout print et début du if

--- parser.py
+++ parser.py
@@ -5,110 +5,86 @@ import AST
 
 vars = {}
 
-
 def p_programme_statement(p):
-    ''' programme : statement
-            | declaration '''
+    ''' programme : statement '''
     p[0] = AST.ProgramNode(p[1])
-
 
 def p_programme_recursive(p):
     ''' programme : statement ';' programme '''
-    p[0] = AST.ProgramNode([p[1]] + p[3].children)
-
-def p_programme_recursiveType(p):
-    ''' programme : declaration ';' programme '''
-    p[0] = AST.ProgramNode([p[1]] + p[3].children)
-
+    p[0] = AST.ProgramNode([p[1]]+p[3].children)
 
 def p_statement(p):
     ''' statement : assignation
-                | structure '''
+        | structure '''
     p[0] = p[1]
+    	
+def p_statement_print(p):
+    ''' statement : PRINT expression '''
+    p[0] = AST.PrintNode(p[2])
 
-def p_declaration(p):
-    ''' declaration : int IDENTIFIER '''
-    p[0] = p[2]
-
-
-def p_statement_slark(p):
-    ''' statement : SLARK expression '''
-    p[0] = AST.SlarkNode(p[2])
-
-def p_statement_int(p):
-    ''' statement : INT statement '''
-    p[0] = AST.IntNode(p[2])
-
-
-def p_structure_while(p):
+def p_structure_While(p):
     ''' structure : WHILE expression '{' programme '}' '''
-    p[0] = AST.WhileNode([p[2], p[4]])
+    p[0] = AST.WhileNode([p[2],p[4]])
 
-
-def p_structure_ahlurglgr(p):
-    ''' structure : AHLURGLGR expression '{' programme '}' '''
-    p[0] = AST.IfNode([p[2], p[4]])
-
-def p_structure_lurgglbr(p):
-    ''' structure : LURGGLBR '{' programme '}' '''
-    p[0] = AST.ElseNode([p[3]])
+def p_structure_IF(p):
+    ''' structure : SCARGIL expression '{' programme '}' '''
+    p[0] = AST.ScargilNode([p[2], p[4]])
 
 def p_expression_op(p):
     '''expression : expression ADD_OP expression
             | expression MUL_OP expression'''
     p[0] = AST.OpNode(p[2], [p[1], p[3]])
-
-
+    	
 def p_expression_num_or_var(p):
     '''expression : NUMBER
         | IDENTIFIER '''
     p[0] = AST.TokenNode(p[1])
-
-
+    	
+def p_expression_paren(p):
+    '''expression : '(' expression ')' '''
+    p[0] = p[2]
+    	
 def p_minus(p):
     ''' expression : ADD_OP expression %prec UMINUS'''
     p[0] = AST.OpNode(p[1], [p[2]])
 
 
 def p_assign(p):
-    ''' assignation : IDENTIFIER '=' expression '''
-    p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+    ''' assignation : IDENTIFIER EQUAL expression '''
+    p[0] = AST.AssignNode([AST.TokenNode(p[1]),p[3]])
 
 
 def p_error(p):
     if p:
-        print("Syntax error in line %d" % p.lineno)
+        print ("Syntax error in line %d" % p.lineno)
         yacc.errok()
     else:
-        print("Sytax error: unexpected end of file!")
+        print ("Sytax error: unexpected end of file!")
 
 
 precedence = (
     ('left', 'ADD_OP'),
     ('left', 'MUL_OP'),
-    ('right', 'UMINUS'),
+    ('right', 'UMINUS'),  
 )
-
 
 def parse(program):
     return yacc.parse(program)
 
-
 yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
-    import sys
-
+    import sys 
+    	
     prog = open(sys.argv[1]).read()
     result = yacc.parse(prog)
     if result:
-        print(result)
-
+        print (result)
+            
         import os
-
         graph = result.makegraphicaltree()
-        name = os.path.splitext(sys.argv[1])[0] + '-ast.pdf'
-        graph.write_pdf(name)
-        print("wrote ast to", name)
+        name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
+        graph.write_pdf(name) 
+        print ("wrote ast to", name)
     else:
-        print("Parsing returned no result!")
+        print ("Parsing returned no result!")
