43f0336f546323f4dbbf618ef16657ec6b7e7036
cyrilmanuel <cyriljeanneret@gmail.com>
2017-02-12 09:23:12 +0100

    compiler op , manque juste les indentations et points virgule correcte

--- CodePropre.c
+++ CodePropre.c
@@ -1,2 +1,28 @@
-a=10.0;
+int a=2.0
+double B=3.0
+float c=5.0
+a=10.0
+a=b*2.0
 print(b);
+if(a*2.0 >= 2.0) 
+ { 
+   print(badas);
+a=20.0
+ } 
+for(int i=0.0;i == 2.0;i=i*1.0) 
+ { 
+ print(i);
+ } 
+
+switch(a){
+case 2.0:
+print(a);
+break; 
+case 3.0:
+print(a);
+a=a*2.0
+break; 
+case default:
+print(b);
+break; 
+}
--- compiler.py
+++ compiler.py
@@ -10,11 +10,11 @@ operations = {
 }
 
 vars = {}
-
+suffix = "   "
 vartypes = {
     'brglmurgl': 'int',
     'lurgglbr': 'float',
-    'ahlurglgr': 'string',
+    'ahlurglgr': 'double',
 }
 
 conditions = {
@@ -39,36 +39,67 @@ op = {
 def compile(self):
     c_code = ""
     for c in self.children:
-        c_code += c.compile()
+        c_code += c.compile() + "\n"
     return c_code
 
 
 @addToClass(AST.TokenNode)
 def compile(self):
-    if isinstance(self.tok, str):
-        try:
-            return vars[self.tok]
-        except KeyError:
-            print("∗∗∗ Error: variable %s undefined!" % self.tok)
+   # if isinstance(self.tok, str):
+    #    try:
+     #       return vars[self.tok]
+      #  except KeyError:
+       #     print("∗∗∗ Error: variable %s undefined!" % self.tok)
     return self.tok
 
 
 @addToClass(AST.OpNode)
 def compile(self):
     c = [ch.compile() for ch in self.children]
-    return "{} {} {}".format(c[0], op[self.op], c[1])
+    return "{}{}{}".format(c[0], op[self.op], c[1])
 
 
 @addToClass(AST.AssignNode)
 def compile(self):
-    vars[self.children[0].tok] = self.children[1].compile()
-    return "{}={};\n".format(self.children[0].tok, self.children[1].compile())
+    return "{}={}".format(self.children[0].tok, self.children[1].compile())
 
 
+@addToClass(AST.DeclarationNode)
+def compile(self):
+    return "{} {}".format(vartypes.get(self.children[0].tok), self.children[1].compile())
 
 @addToClass(AST.PrintNode)
 def compile(self):
-    return "print({});\n".format(self.children[0].compile().tok)
+    return "print({});".format(self.children[0].compile().tok)
+
+@addToClass(AST.ScargilNode)
+def compile(self):
+    return "if({}) \n {{ \n   {} }} ".format(self.children[0].compile(), self.children[1].compile())
+
+@addToClass(AST.ConditionNode)
+def compile(self):
+    return "{} {} {}".format(self.children[0].compile(), conditions.get(self.children[1].compile()),
+                             self.children[2].compile())
+
+@addToClass(AST.ForNode)
+def compile(self):
+    return "for({};{};{}) \n {{ \n {} }} \n".format(self.children[0].compile(),
+        self.children[1].compile(), self.children[2].compile(), self.children[3].compile())
+
+@addToClass(AST.SwitchNode)
+def compile(self):
+    return "switch({}){{\n{}}}".format(self.children[0].compile(), self.children[1].compile())
+
+@addToClass(AST.CaseNode)
+def compile(self):
+    c_code = ""
+    i = 0
+    while i < len(self.children):
+        c_code += "case {}:\n".format(self.children[i].compile())
+        c_code += self.children[i+1].compile() + "break; \n"
+        i += 2
+
+    return c_code
 
 
 if __name__ == '__main__':
